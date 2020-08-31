a46dc85bc03995cf64fea9bf5ac6374a7425cba8
Victoria Antonova <gershovavictoria@gmail.com>
2020-08-31 14:26:48 +0200

    Create input event listener for rss link field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 const App = () => {
-  
+  const state = {
+    form: {
+      processState: 'filling',
+      isValid: true,
+      errors: [],
+      fields: {
+        rSSLink: '',
+      },
+    },
+    feeds: [],
+    posts: [],
+  };
+
+  const newRSSLinkForm = document.querySelector('.rssLink');
+  const rSSLinkInput = document.getElementById('rSSLink');
+
+  rSSLinkInput.addEventListener('input', (e) => {
+    const { target: { value } } = e;
+    state.form.fields.rSSLink = value;
+  })
+
 };
 
 export default App;
