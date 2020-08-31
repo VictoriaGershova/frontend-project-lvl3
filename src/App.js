10b0697f7488f99e699fb88ef733a27f07c6b7dc
Victoria Antonova <gershovavictoria@gmail.com>
2020-08-31 17:53:42 +0200

    add validation with YUP lib for the form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  rSSLink: yup.string().required()
+    .url()
+    .when('$feeds', (feeds, schema) => (
+      !feeds ? schema : schema.notOneOf(feeds, 'Must be uniq url')))
+});
+
 const App = () => {
   const state = {
     form: {
@@ -12,13 +21,36 @@ const App = () => {
     posts: [],
   };
 
-  const newRSSLinkForm = document.querySelector('.rssLink');
+  const handleSubmit = () => {
+    state.form.processState = 'validation';
+    const { feeds } = state;
+    schema
+      .validate(state.form.fields, { abortEarly: false, context: { feeds } })
+      .then(() => {
+        state.form.isValid = true;
+        state.form.processState = 'processing';
+        state.errors = [];
+        state.feeds.push(state.form.fields.rSSLink);
+      })
+      .catch((err) => {
+        state.form.isValid = false;
+        state.form.processState = 'filling';
+        state.errors = err.errors;
+      });
+  };
+
+  const newRSSLinkForm = document.querySelector('.rss-form');
   const rSSLinkInput = document.getElementById('rSSLink');
 
   rSSLinkInput.addEventListener('input', (e) => {
     const { target: { value } } = e;
     state.form.fields.rSSLink = value;
   })
+
+  newRSSLinkForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleSubmit();
+  });
 
 };
 
