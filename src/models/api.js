1571fe37bd4cedb03ef71700a57c2f10487a2ab5
Victoria Antonova <gershovavictoria@gmail.com>
2020-09-22 14:04:36 +0200

    swap then and catch blocks in get request

--- src/models/api.js
+++ src/models/api.js
@@ -6,16 +6,15 @@ const corsAPIUrl = `https://${corsAPIHost}/`;
 const getRSSData = (link) => {
   const url = `${corsAPIUrl}${link}`;
   return axios.get(url, { timeout: 10000 })
-    .catch((err) => {
+    .then((res) => {
+      const { data } = res;
+      return data
+    }).catch((err) => {
       err.name = 'NetworkError';
       if (err.response) {
         err.message = `network_${err.response.status}`;
       }
       throw err;
-    })
-    .then((res) => {
-      const { data } = res;
-      return data
     });
 };
 
